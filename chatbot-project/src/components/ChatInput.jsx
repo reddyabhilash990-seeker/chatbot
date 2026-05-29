3203e9414e5cbca9f165f2a1609316831866dea8
Abhilash Reddy <reddyabhilash990@gmail.com>
2026-05-29 09:54:14 +0530

    Updated chatbot components and styles

---
+++ chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,81 @@
+import { useState} from 'react'
+import { Chatbot } from 'supersimpledev';
+import LoadingSpinner from './assets/loading-spinner.gif';
+
+
+export function ChatInput({chatMessages, setChatMessages}) {
+
+        const [inputText, setInputText] = useState('');
+        function  saveInputText(event) {
+          setInputText(event.target.value)
+
+        }
+
+        
+        async function sendMessage() {
+
+
+
+            setInputText(' ');
+            const newChatMessages = [
+              ...chatMessages ,                //spreadOperator
+              {
+                message: inputText ,
+                sender: 'user' ,
+                id: crypto.randomUUID() 
+              }
+
+            ];
+            setChatMessages([
+                ...newChatMessages,                //spreadOperator
+                {
+                  message: <img src={LoadingSpinner} className="loading-spinner" /> ,
+                  sender: 'robot' ,
+                  id: crypto.randomUUID() 
+                }
+            ]);
+              const response = await Chatbot.getResponseAsync(inputText);
+            setChatMessages([
+                ...newChatMessages,
+                {
+                  message: response,
+                  sender: 'robot' ,
+                  id : crypto.randomUUID()
+                }
+            ]);
+            
+            
+        }
+
+
+
+          
+
+          
+
+          
+          
+        
+
+
+      
+      
+        
+
+
+        return (
+            <div className="chat-input-container">
+              <input 
+                placeholder="send a message to chatbox" 
+                size = "30"
+                onChange={saveInputText} 
+                value={inputText}
+                className="chat-input"
+              />
+              <button
+                onClick = {sendMessage} 
+                className="send-button"
+              >Send</button>
+            </div>
+        );
+}
